df2191bd8de239743616da833ea16fd3281a6121
justinavickers <justinav38@gmail.com>
2019-01-24 10:52:52 -0600

    fixed my json file

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -4,10 +4,10 @@
 //  renderJournalEntries(journalEntries)// What should happen when we finally have the array?
 // })
 
-const API = {
-  getJournalEntries () {
-    console.log("get journal entries running")
-      return fetch("http://localhost:3000/entries")
-          .then(response => response.json())
-  }
-}
+// const API = {
+//   getJournalEntries () {
+//     console.log("get journal entries running")
+//       return fetch("http://localhost:3000/entries")
+//           .then(response => response.json())
+//   }
+// }
